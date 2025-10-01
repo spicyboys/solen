1767f10bc7001d99df875d7ff364b0d7ae14c572
Robert Lynch <robertlynch2012@gmail.com>
2025-10-01 16:48:45 -0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 use std::env;
 
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::prelude::*;
+use serenity::{
+    all::{ChannelId, CreateMessage, GuildChannel, MessageBuilder},
+    async_trait,
+    prelude::*,
+};
 
 struct Handler;
 
+// Spicy Boys
+static SPICY_GAMES: ChannelId = ChannelId::new(1406825680741597286);
+static GAMES_CHAT: ChannelId = ChannelId::new(935677093642133564);
+
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
-            }
+    async fn thread_create(&self, ctx: Context, thread: GuildChannel) {
+        if thread.parent_id != Some(SPICY_GAMES) {
+            return;
         }
+
+        let Some(owner_id) = thread.owner_id else {
+            return;
+        };
+
+        let message_content = MessageBuilder::new()
+            .mention(&owner_id)
+            .push(" has created a new thread in ")
+            .channel(SPICY_GAMES)
+            .push(": ")
+            .channel(thread.id)
+            .build();
+        let message = CreateMessage::new().content(message_content);
+        let _ = GAMES_CHAT.send_message(ctx.http, message).await;
     }
 }
 
@@ -21,16 +39,13 @@ impl EventHandler for Handler {
 async fn main() {
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILDS;
 
-    // Create a new instance of the Client, logging in as a bot.
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
-    // Start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
     }
