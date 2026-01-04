fb97417aa98682a9daf17051124b8f1ab6d2aa27
Robert Lynch <robertlynch2012@gmail.com>
2026-01-03 22:49:50 -0800

    Small formatting

--- src/jobs/birthday.rs
+++ src/jobs/birthday.rs
@@ -1,21 +1,23 @@
+use crate::{channels, jobs::JobContext, models::birthdays};
 use anyhow::Result;
 use chrono::Datelike;
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use chrono_tz::US::Central;
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use serenity::all::{CreateMessage, MessageBuilder, UserId};
-use crate::{channels, jobs::JobContext, models::birthdays};
 
 pub async fn send_birthday_message(ctx: JobContext) -> Result<()> {
     let today = chrono::Utc::now().with_timezone(&Central).date_naive();
-    println!("Checking birthdays for {}-{}...", today.month(), today.day());
+    println!(
+        "Checking birthdays for {}-{}...",
+        today.month(),
+        today.day()
+    );
+    
     let birthdays = birthdays::Entity::find()
         .filter(birthdays::Column::Day.eq(today.day() as i16))
         .filter(birthdays::Column::Month.eq(today.month() as i16))
         .all(&ctx.db)
-        .await;
-
-    println!("{:?}", birthdays);
-    let birthdays = birthdays?;
+        .await?;
 
     if birthdays.is_empty() {
         return Ok(());
@@ -29,7 +31,9 @@ pub async fn send_birthday_message(ctx: JobContext) -> Result<()> {
             .mention(&user_id)
             .build();
 
-        channels::CONFIDENTIAL.send_message(&ctx.discord_http, CreateMessage::new().content(content)).await?;
+        channels::CONFIDENTIAL
+            .send_message(&ctx.discord_http, CreateMessage::new().content(content))
+            .await?;
     }
     Ok(())
 }
