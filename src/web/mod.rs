28a34421e2155887d77428a95a85476becfa2bf6
Robert Lynch <robertlynch2012@gmail.com>
2026-08-02 11:33:24 -0700

    Small layer to avoid 404 on /

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -11,9 +11,13 @@ use reqwest::Client as HttpClient;
 use topcoat::{
     Result,
     asset::{AssetBundle, RouterBuilderAssetExt},
+    context::CxBuilder,
     cookie::RouterBuilderCookieExt,
     font::{Font, fontsource::fontsource_font},
-    router::{Router, RouterBuilderDiscoverExt, layout, module_router},
+    router::{
+        Body, Next, Response, Router, RouterBuilderDiscoverExt, error::redirect, layer, layout,
+        module_router, uri,
+    },
     session::{RouterBuilderSessionExt, SessionConfig},
     tailwind,
     view::view,
@@ -66,5 +70,19 @@ async fn root_layout(slot: Result) -> Result {
             </head>
             <body>(slot?)</body>
         </html>
+    }
+}
+
+// We don't serve anything on root right now, this just sends logged in people
+// to soundboards so they don't get a 404
+#[layer]
+async fn redirect_logged_in(cx: &mut CxBuilder, body: Body, next: Next<'_>) -> Result<Response> {
+    let uri = uri(cx);
+    if uri.path() != "/" {
+        Ok(next.run(cx, body).await?)
+    } else if auth::current_user_id(cx).await.is_some() {
+        Err(redirect("/soundboards").into())
+    } else {
+        Err(redirect("/login").into())
     }
 }
